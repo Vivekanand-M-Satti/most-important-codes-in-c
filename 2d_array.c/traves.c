0f4e0b1a970cf5181c2745b61783369cdaf5bdef
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-01 17:20:55 +0000

    update

--- 2d_array.c/traves.c
+++ 2d_array.c/traves.c
@@ -1,3 +1,5 @@
+//----------------------------method one boss
+#if 0
 #include<stdio.h>
 #include<stdlib.h>
 int main()
@@ -30,6 +32,33 @@ int main()
         for(int j=0;j<3;j++)
         {
             printf("%d",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+#endif
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *arr[3]; 
+    for(int i=0;i<3;i++)
+    {
+        arr[i]=malloc(3*sizeof(int));
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+  
+     for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d",arr[j][i]);
         }
         printf("\n");
     }
