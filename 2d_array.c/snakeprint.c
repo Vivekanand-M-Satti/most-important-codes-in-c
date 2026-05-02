477c3b7c17d960172046114510fcc066ef78d627
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-02 10:01:03 +0000

    update

---
+++ 2d_array.c/snakeprint.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int r=3,c=3;
+    int arr[r][c];
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(i==1)
+            {
+                printf("%d ",arr[c-j-1][i]);
+            }
+            else{
+            printf("%d ",arr[j][i]);
+            }
+        }
+    }
+}
