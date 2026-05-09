c75d6d75b343d84986a020c05ff65dce93ef0a29
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-09 17:12:55 +0000

    update

--- quizz/quiz1.c
+++ quizz/quiz1.c
@@ -1,17 +1,26 @@
 //vvnat is the output of the fonowing cod
-#include<stdio.h>
+// #include<stdio.h>
 
+// int main()
+// {
+
+// int i; unsigned m=-1;
+// int k=(m<4);
+
+// for (i = -1; i < sizeof(i); i++)
+// {
+
+// printf("%d\n", i);
+
+
+// }
+// printf("%d\n", k);
+// }
+
+
+#include<stdio.h>
 int main()
 {
-
-int i;
-
-for (i = -1; i < sizeof(i); i++)
-{
-
-printf("%d\n", i);
-
-return 0;
-}
-
+    int i=-1;
+    printf("%d",i);
 }
